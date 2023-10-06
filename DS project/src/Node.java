747890550514ce19072fb305f289b1e4d051b341
Miriam <88946876+miriamsm@users.noreply.github.com>
2023-10-06 14:26:06 +0300

    Update Node.java

--- DS project/src/Node.java
+++ DS project/src/Node.java
@@ -1,11 +1,42 @@
-public class Node { 
-    Contact data;
-    Node next;
-    Node prev;
+public class Node<T> {
+public T data;
+public Node<T> next;
+public Node<T> previous;
 
-    public Node(Contact data) {
+public Node () {
+data = null;
+next = null;
+previous = null;
+}
+
+public Node (T val) {
+data = val;
+next = null;
+previous= null;
+}
+
+public T getData() {
+        return this.data;
+}
+
+public void setData(T data) {
         this.data = data;
-        this.next = null;
-        this.prev=null;
-    }
+}
+
+public Node<T> getNext() {
+        return this.next;
+}
+
+public void setNext(Node<T> next) {
+        this.next = next;
+}
+
+public Node<T> getPrevious() {
+        return this.previous;
+}
+
+public void setPrevious(Node<T> previous) {
+        this.previous = previous;
+}
+
 }

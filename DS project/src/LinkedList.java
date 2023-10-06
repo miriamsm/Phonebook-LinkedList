f4bfa4b9883f4df1ae3f80e960ad3d1530b2062b
Miriam <88946876+miriamsm@users.noreply.github.com>
2023-10-06 14:22:58 +0300

    Update LinkedList.java

--- DS project/src/LinkedList.java
+++ DS project/src/LinkedList.java
@@ -1,9 +1,84 @@
-public class <T> LinkedList {
-    private LinkedList<Contact> contacts;
+public class LinkedList<T>{
+    private Node<T> head;
+    private Node<T> current;
 
-     public LinkedList() {
-        contacts = new LinkedList<>();
+    public LinkedList(){
+        head=current=null;
     }
 
-    
+public boolean empty(){
+    return head==null;
 }
+
+public boolean last(){
+    return current.next==null;
+}
+
+public boolean full () {
+return false; 
+}
+
+public void findfirst () {
+current = head;
+}
+
+public void findnext () {
+current = current.next; 
+}
+
+public T retrieve () {
+return current.data; 
+}
+
+public void update (T val) {
+current.data = val; 
+}
+
+
+public void insert(T val) {
+        Node<T> tmp = new Node<T>(val);
+        if(empty()) {
+        current = head = tmp;
+        }
+        else {
+        tmp.next = current.next;
+        tmp.previous = current;
+        if(current.next != null)
+        current.next.previous = tmp;
+        current.next = tmp;
+        current = tmp;
+        }
+}
+        
+
+public void remove() {
+            if(current == head) {
+            head = head.next;
+            if(head != null)
+            head.previous = null;
+            }
+            else {
+            current.previous.next = current.next;
+            if(current.next != null)
+            current.next.previous = current.previous;
+            }
+            if(current.next == null)
+            current = head;
+            else
+            current = current.next;
+}
+
+public void findPrevious() {
+                Node<T> tmp = head;
+                while(tmp.next != current)
+                tmp = tmp.next;
+                current = tmp;
+                }
+
+                public void findLast() {
+                    while(current.next != null)
+                    current = current.next;
+                    }
+}
+            
+    

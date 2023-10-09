f16cc56afc5dd71a098d9a1619df9f666a9b295f
Xqbae <145024980+Xqbae@users.noreply.github.com>
2023-10-09 11:55:45 +0300

    Update Node.java

--- DS project/src/Node.java
+++ DS project/src/Node.java
@@ -1,42 +1,42 @@
 public class Node<T> {
-public T data;
-public Node<T> next;
-public Node<T> previous;
+        public T data;
+        public Node<T> next;
+        public Node<T> previous;
+        
+        public Node () {
+                data = null;
+                next = null;
+                previous = null;
+        }
+        
+        public Node (T val) {
+                data = val;
+                next = null;
+                previous= null;
+        }
 
-public Node () {
-data = null;
-next = null;
-previous = null;
-}
+        public T getData() {
+                return this.data;
+        }
 
-public Node (T val) {
-data = val;
-next = null;
-previous= null;
-}
+        public void setData(T data) {
+                this.data = data;
+        } 
 
-public T getData() {
-        return this.data;
-}
+        public Node<T> getNext() {
+                return this.next;
+        }
 
-public void setData(T data) {
-        this.data = data;
-}
+        public void setNext(Node<T> next) {
+               this.next = next;
+        }
 
-public Node<T> getNext() {
-        return this.next;
-}
+        public Node<T> getPrevious() {
+                return this.previous;
+        }
 
-public void setNext(Node<T> next) {
-        this.next = next;
-}
-
-public Node<T> getPrevious() {
-        return this.previous;
-}
-
-public void setPrevious(Node<T> previous) {
-        this.previous = previous;
-}
+        public void setPrevious(Node<T> previous) {
+                this.previous = previous;
+        }
 
 }

65fc003ec0e515107ce3e309b26c76ab9905f238
Riyam <88596299+RIYAM-M-S@users.noreply.github.com>
2023-10-12 12:06:08 +0300

    linked

--- DS project/src/LinkedList.java
+++ DS project/src/LinkedList.java
@@ -1,152 +1,93 @@
-public class LinkedList<T>{
+public class LinkedList<T> {
     private Node<T> head;
     private Node<T> current;
 
-    public LinkedList(){
-        head=current=null;
+    public LinkedList() {
+        head = current = null;
     }
 
-    public boolean empty(){
-     return head==null;
+    public boolean empty() {
+        return head == null;
     }
 
-    public boolean last(){
-      return current.next==null;
+    public boolean last() {
+        return current == null || current.next == null;
     }
 
-    public boolean full () {
-      return false; 
+    public void findfirst() {
+        current = head;
     }
 
-    public void findfirst () {
-     current = head;
+    public void findnext() {
+        if (current != null) {
+            current = current.next;
+        }
     }
 
-    public void findnext () {
-     current = current.next; 
+    public T retrieve() {
+        return (current != null) ? current.getData() : null;
     }
-
-    public T retrieve () {
-     return current.getData(); 
-    }
-
-    public void update (T val) {
-     current.data = val; 
-    }
-
-
-    // public void insert(T val) {
-    //     Node<T> tmp = new Node<>(val);
-    //     if(empty()) {
-    //     current = head = tmp;
-    //     }else {
-    //     tmp.next = current.next;
-    //     tmp.previous = current;
-    //     if(current.next != null){
-    //     current.next.previous = tmp;
-    //     current.next = tmp;
-    //     current = tmp;}
-    //     }
-    // }
-
 
     public void insert(T data) {
-       
-        // Create Node
         Node<T> node = new Node<>(data);
-       
-        // case 1: empty set
-        if(this.empty()) {
-            head = node;
-            current=head;
-            return;
+
+        if (empty()) {
+            head = current = node;
+        } else {
+            current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = node;
         }
-        // otherwise move to end of the set and store it
-        Node<T> current = head;
-       
-        // Reach to the last node
-        while(current.getNext() != null) {
-            current = current.getNext();
-        }
-       
-        // end of set reached, insert here
-        current.setNext(node);
     }
-        
-
-    // public void remove() {
-    //         if(current == head) {
-    //         head = head.next;
-    //         if(head != null)
-    //         head.previous = null;
-    //         }
-    //         else {
-    //         current.previous.next = current.next;
-    //         if(current.next != null)
-    //         current.next.previous = current.previous;
-    //         }
-    //         if(current.next == null)
-    //         current = head;
-    //         else
-    //         current = current.next;
-    // }
-
 
     public void remove(T data) {
-       
-        // first check if empty
-        if(this.empty()) {
+        if (empty()) {
             return;
         }
-       
-        // Case 2: exist at the head of set
-        if(head.getData().equals(data)) {
-            head = head.getNext();
-            return;
-        }
-       
-        Node<T> current = head, pre = null;
-       
-        // end of set
-        while(current.next!= null) {
-           
-            if(current.getData().equals(data)) {
-                pre.setNext(current.getNext());
-                return;
-            }
-           
-            pre = current;
-            current = current.getNext();            
-        }
-       
-        // if we reach here, means element does not exist
-        return;
-    }
-   
 
-    public void findPrevious() {
-        Node<T> tmp = head;
-            while(tmp.next != current)
-                tmp = tmp.next;
-            current = tmp;
+        if (head.getData().equals(data)) {
+            head = head.next;
+            current = head;
+            return;
+        }
+
+        current = head;
+        Node<T> previous = null;
+
+        while (current != null && !current.getData().equals(data)) {
+            previous = current;
+            current = current.next;
+        }
+
+        if (current != null) {
+            previous.next = current.next;
+            current = previous;
+        }
     }
+
 
     public void findLast() {
-        while(current.next != null)
+        if (head == null) {
+            current = null;
+            return;
+        }
+
+        current = head;
+        while (current.next != null) {
             current = current.next;
+        }
     }
 
-    
     public boolean contains(T obj) {
         current = head;
-
-        while (current.next != null) {
-            if (current.data.equals(obj)) {
-                return true; 
+        while (current != null) {
+            if (current.getData().equals(obj)) {
+                return true;
             }
             current = current.next;
         }
-
         return false;
     }
 
@@ -189,4 +130,5 @@ public class LinkedList<T>{
     }
 
 }
+
     
